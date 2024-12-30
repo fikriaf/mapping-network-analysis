d5c83a1c1d46f541e14d829b8e331f58b36ea159
Fikri Armia Fahmi <143471086+fikriaf@users.noreply.github.com>
2024-12-30 13:23:34 +0700

    Update mapping_network.py

--- mapping_network.py
+++ mapping_network.py
@@ -14,26 +14,18 @@ world = gpd.read_file(shapefile_path)
 banten = world[world["shape1"] == 'Banten']
 banten_geom = banten.geometry.iloc[0]
 
-provider = "X"
-lokasi_tower = [
-    (106.39588367887067, -6.109830593420505),
-    (106.42711714995869, -6.232778477997128),
-    (106.4747400809301, -6.2574895930647365),
-    (106.5099226690555,  -6.2222056665356),
-    (106.62083648362552, -6.186709674342691),
-    (106.7264325125655,  -6.275067589565951),
-    (106.77285905125615, -6.304976580692439),
-    (106.19161127463387, -6.6319367876668505),
-    (106.39061922684432, -6.609428575868837),
-    (106.49012320294953, -6.766964350087271)
-]
-
+provider = "Telkomsel"
 
 # ---------------------------------------------------------------------------------------------
-# Simulasi posisi tower provider X dan sampel pengguna provider X
+# Mengumpulkan Data Tower
 print("[Mengumpulkan Lokasi Tower...]")
-tower_positions = tower.random_tower(10)
+tower_positions = tower.get_tower()
 
+print("[Mengumpulkan Data Tower]")
+data_tower = tower.towers
+data_dBm_tower = [int(x['max_signal_rsrp'].replace(' dBm', '')) for x in data_tower]
+
+# Mengumpulkan Data User
 print(f"[Mengumpulkan Data Pengguna Provider {provider}...]")
 data_sinyal = sampel.data(tower_positions, 1000)
 
@@ -46,7 +38,7 @@ geo_df_sinyal = gpd.GeoDataFrame(df_sinyal, geometry=geometry)
 
 
 # ---------------------------------------------------------------------------------------------
-# Membuat graf jaringan dengan NetworkX
+# Algoritma NetworkX
 G = nx.Graph()
 
 # Menambahkan node untuk tower
@@ -57,8 +49,9 @@ for i, (lon, lat) in enumerate(tower_positions):
 for i, row in df_sinyal.iterrows():
     G.add_node(f"Signal_{i}", pos=(row["longitude"], row["latitude"]), 
                type="signal", signal_strength=row["signal_strength"])
-
-# Menambahkan edge berdasarkan jarak, hanya menghubungkan ke tower terdekat
+    
+# ---------------------------------------------------------------------------------------------
+# Algoritma Nearest Neighbor
 edges = []
 for i, row in df_sinyal.iterrows():
     signal_point = Point(row["longitude"], row["latitude"])
@@ -74,22 +67,29 @@ for i, row in df_sinyal.iterrows():
     if closest_tower is not None:
         edges.append((f"Signal_{i}", closest_tower, min_distance))
 
-# Menambahkan edges ke graf
 G.add_weighted_edges_from(edges)
-# Membuat Minimum Spanning Tree (MST) dari graf
+# ---------------------------------------------------------------------------------------------
+
 mst = nx.minimum_spanning_tree(G)
-# Visualisasi peta
 fig, ax = plt.subplots(figsize=(12, 10))
 banten.plot(ax=ax, color="lightgrey", edgecolor="black")
-ax.set_title(f"Persebaran Sinyal Pengguna {provider} di Banten Berbasis Algoritma Network Analisys")
+ax.set_title(f"Persebaran Sinyal Telkomsel di Banten Berbasis GIS\nMenggunakan Algoritma Network Analisys dan Nearest Neighbor")
+
+# ---------------------------------------------------------------------------------------------
 # Plot tower
 for lon, lat in tower_positions:
-    ax.scatter(lon, lat, color="blue", s=100, label="Tower Provider X")
+    ax.scatter(lon, lat, color="blue", s=10, label="Tower Provider Telkomsel")
 # Plot sinyal
 for i, row in df_sinyal.iterrows():
-    color = "green" if row["signal_strength"] >= 80 else \
-            "yellow" if row["signal_strength"] >= 50 else "red"
-    ax.scatter(row["longitude"], row["latitude"], color=color, s=50)
+    lon = row["longitude"]
+    lat = row["latitude"]
+    distances = [Point(t).distance(Point(lon, lat)) for t in tower_positions]
+    min_distance = min(distances)
+    get_dBm_tower = data_dBm_tower[distances.index(min_distance)]
+
+    color = "red" if row["signal_strength"] <= get_dBm_tower else \
+            "yellow" if row["signal_strength"] <= get_dBm_tower+16 else "green"
+    ax.scatter(row["longitude"], row["latitude"], color=color, s=10)
 
 # ---------------------------------------------------------------------------------------------
 # Visualisasi jaringan dengan NetworkX
@@ -97,16 +97,25 @@ print("[Menampilkan Visualisasi...]")
 pos = nx.get_node_attributes(G, "pos")
 nx.draw_networkx_edges(mst, pos, ax=ax, alpha=0.3, edge_color="gray")
 
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+
+# Label kiri
+ax.text(xlim[0] - 10, 0.5, 'Label Kiri: Persebaran Sinyal', fontsize=12, ha='center', va='center', rotation=90)
+
+# Label kanan
+ax.text(xlim[1] + 10, 0.5, 'Label Kanan: Menggunakan Algoritma MST', fontsize=12, ha='center', va='center', rotation=-90)
+
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
 legend_elements = [
-    Line2D([0], [0], marker='o', color='w', label=f'Tower Provider {provider}',
+    Line2D([0], [0], marker='o', color='w', label=f'Tower Provider {provider} (100 tower)',
            markerfacecolor='blue', markersize=10),
-    Line2D([0], [0], marker='o', color='w', label='Sinyal Kuat',
+    Line2D([0], [0], marker='o', color='w', label='User Sinyal Kuat (RSRP >= -80dBm)',
            markerfacecolor='green', markersize=10),
-    Line2D([0], [0], marker='o', color='w', label='Sinyal Sedang',
+    Line2D([0], [0], marker='o', color='w', label='User Sinyal Sedang (-96dBm < RSRP < -80dBm)',
            markerfacecolor='yellow', markersize=10),
-    Line2D([0], [0], marker='o', color='w', label='Sinyal Lemah',
+    Line2D([0], [0], marker='o', color='w', label='User Sinyal Lemah (RSRP <= -96dBm)',
            markerfacecolor='red', markersize=10),
 ]
 plt.legend(handles=legend_elements, loc="upper left")
